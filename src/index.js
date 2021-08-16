5b1072e858821b6ce7292cbb9d662e2089c5c253
Diego Solis <disc3110@gmail.com>
2021-08-16 18:18:07 -0500

    refactor hooks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import TodoContainer from './components/TodoContainer'
-import './App.css'
+import TodoContainer from './classBased/components/TodoContainer'
+import './classBased/App.css'
 
 
 ReactDom.render(
